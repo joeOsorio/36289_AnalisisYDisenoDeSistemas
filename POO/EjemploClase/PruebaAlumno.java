66693623b1840b9de403d345ffc8dd5c1337a33f
Joe Osorio <joe.osorio.oso@gmail.com>
2026-03-10 10:11:35 -0700

    Listo lo de POO

--- POO/EjemploClase/PruebaAlumno.java
+++
@@ -1,7 +0,0 @@
-public class PruebaAlumno {
-    public static void main(String [] args)
-    {  
-        Alumno alumno1= new Alumno("Juan", "Luis", "Martinez", "Lopez",  22, 10, 2002,  121212,  "Ingenieria en computacion", 3);
-        System.out.println(alumno1);
-    }
-}
--- POO/pruebaEncapsulamiento.java
+++
@@ -1,8 +0,0 @@
-public class pruebaEncapsulamiento{
-    public static void main(String [] args){
-        Box5 miCaja = new Box5();
-        miCaja.ancho = 10;
-
-        System.out.printf("El valor del ancho de la caja es %f%n", miCaja.getAncho());
-    }
-}
